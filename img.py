423064a18b53f681797a5743edd9955086e0cfc9
Umair <114747567+ummayrr@users.noreply.github.com>
2024-02-21 18:37:19 +0500

    404 check

--- img.py
+++ img.py
@@ -2,16 +2,20 @@ import requests
 import json
 import os
 import glob
+import mimetypes
 
 cse_id = ""
 api_key = ""
 
-#delete 20 images
-for i in range(1, 21):
-    for file in glob.glob(f'image{i}.*'):
-        if os.path.isfile(file):
-            os.remove(file)
-            print(f'Deleted: {file}')
+download_directory = "downloads"
+
+if not os.path.exists(download_directory):
+    os.makedirs(download_directory)
+
+for file in glob.glob(os.path.join(download_directory, '*')):
+    if os.path.isfile(file):
+        os.remove(file)
+        print(f'Deleted: {file}')
 
 num_files = len([name for name in os.listdir('.') if os.path.isfile(name) and name.startswith('text') and not name.endswith('original.txt')])
 
@@ -19,23 +23,28 @@ for i in range(1, num_files+1):
     with open(f'text{i}original.txt', 'r') as f:
         search_term = f.read().strip()
 
-    url = f"https://www.googleapis.com/customsearch/v1?q={search_term}&num=1&start=1&searchType=image&key={api_key}&cx={cse_id}"
+    start_index = 1
+    while True:
+        url = f"https://www.googleapis.com/customsearch/v1?q={search_term}&num=1&start={start_index}&searchType=image&key={api_key}&cx={cse_id}"
 
-    response = requests.get(url)
-    response.raise_for_status()
+        response = requests.get(url)
+        response.raise_for_status()
 
-    search_results = response.json()
-    image_url = search_results['items'][0]['link']
+        search_results = response.json()
+        image_url = search_results['items'][0]['link']
 
-    # determining extension
-    file_extension = os.path.splitext(image_url)[1]
-    if file_extension == '':
-        file_extension = '.jpg'
-    filename = f'image{i}{file_extension}'
+        response = requests.get(image_url)
+        if response.status_code == 404:
+            start_index += 1
+            continue
 
-    # downloading image
-    response = requests.get(image_url)
-    with open(filename, 'wb') as out_file:
-        out_file.write(response.content)
+        content_type = response.headers['content-type']
+        extension = mimetypes.guess_extension(content_type)
 
-    print('Image downloaded:', filename)
+        filename = f'image{i}{extension}'
+
+        with open(os.path.join(download_directory, filename), 'wb') as out_file:
+            out_file.write(response.content)
+
+        print('Image downloaded:', os.path.join(download_directory, filename))
+        break
