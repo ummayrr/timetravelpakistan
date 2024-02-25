04c93eaff0bfa0f8d027fc5f050e7495712584cb
Umair <umairnawaz184@gmail.com>
2024-02-25 14:56:33 +0500

    interval randomized

--- app.js
+++ app.js
@@ -1,49 +1,93 @@
 import express from "express";
-import { CronJob } from "cron";
-import dotenv from "dotenv";
+import { CronJob, CronTime } from "cron";
 import { IgApiClient } from "instagram-private-api";
 import { promisify } from "util";
-import { readFile } from "fs";
-
-const app = express();
-const readFileAsync = promisify(readFile);
+import { readFile, readdirSync } from "fs";
+import dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env.INSTA_USERNAME);
+const app = express();
+const readFileAsync = promisify(readFile);
+
 app.use(express.static("public"));
-app.listen(process.env.PORT || 3000, function () {});
+app.listen(3000, function () {});
 
 app.get("/", function (request, response) {
-  response.send('up and running');
+  response.send('chal raha hai bhai');
 });
 
-async function instagramPost() {
-  try {
-    console.log("Posting to Instagram begins..");
-    const { username, password } = process.env;
-    const ig = new IgApiClient();
-    const caption = "Enter caption here";
-    ig.state.generateDevice(username);
-    const user = await ig.account.login(username, password);
-    const path = `todaysPost.jpg`;
-    const published = await ig.publish.photo({
-      file: await readFileAsync(path),
-      caption: caption,
-    });
-    console.log("Posted to Instagram!");
-  } catch (error) {
-    console.log(error);
+let imageIndex = 1;
+
+const username = process.env.INSTA_USERNAME;
+const password = process.env.INSTA_PASSWORD;
+
+function calculateInterval(totalPosts) {
+  let interval; 
+  if (totalPosts >= 14) {
+    interval = 60; // 1 
+  } else if (totalPosts >= 10) {
+    interval = 90; // 1.5 
+  } else if (totalPosts >= 8) {
+    interval = 120; // 2 
+  } else if (totalPosts >= 6) {
+    interval = 150; // 2.5 
+  } else if (totalPosts >= 4) {
+    interval = 180; // 3 
+  } else if (totalPosts == 3) {
+    interval = 210; // 3.5 
+  } else if (totalPosts == 2) {
+    interval = 300; // 5 
+  } else {
+    interval = 330; // 5.5 
   }
+
+  //5-20 min
+  const randomInterval = Math.floor(Math.random() * (interval + 20 - interval + 5) + interval + 5);
+  return randomInterval;
 }
 
-// every minute
-let dailyPost = new CronJob(
-  "* * * * *",
+let totalPosts = readdirSync('edited').length;
+let randomInterval = calculateInterval(totalPosts);
+let postEveryMinute = new CronJob(
+  `*/${randomInterval} * * * *`,
   function () {
-    console.log("Auto post to Instagram begins..");
+    console.log("cron shuru bhai");
     instagramPost();
   },
   true
 );
 
-dailyPost.start();
-console.log('Successfully started, now posting to Instagram every minute!');
+async function instagramPost() {
+  try {
+    console.log("kaam karne ki koshish start bhai");
+    const ig = new IgApiClient();
+    ig.state.generateDevice(username);
+    const user = await ig.account.login(username, password);
+
+    const imagePath = `edited/image${imageIndex}_edited.jpg`;
+    const captionPath = `text${imageIndex}.txt`;
+    const caption = await readFileAsync(captionPath, 'utf8');
+
+    const published = await ig.publish.photo({
+      file: await readFileAsync(imagePath),
+      caption: caption,
+    });
+    console.log("kaam hogaya bhai");
+
+    imageIndex++;
+    totalPosts = readdirSync('edited').length;
+    randomInterval = calculateInterval(totalPosts);
+
+    postEveryMinute.setTime(new CronTime(`*/${randomInterval} * * * *`));
+
+    // agli post
+    console.log(`agli post after ${randomInterval} minutes`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+postEveryMinute.start();
+console.log(`bilkul shuru wala shuru bhai, agli post after ${randomInterval} minutes`);
+
